b4be6fc2dfc9f50bb9cf667ae5def3dd3a17c2f0
BialekPL <mbialecki157@gmail.com>
2022-06-10 15:11:34 +0200

    Added ps1 scripts for Windows users

---
+++ labs/1-Initial-Setup/scripts/create-terraform-storage.ps1
@@ -0,0 +1,13 @@
+
+
+$RESOURCE_GROUP_NAME="devops-journey-rg"
+$STORAGE_ACCOUNT_NAME="devopsjourneyazuredevops"
+
+# Create Resource Group
+az group create -l northeurope -n $RESOURCE_GROUP_NAME
+
+# Create Storage Account
+az storage account create -n $STORAGE_ACCOUNT_NAME -g $RESOURCE_GROUP_NAME -l northeurope --sku Standard_LRS
+
+# Create Storage Account blob
+az storage container create  --name tfstate --account-name $STORAGE_ACCOUNT_NAME
